17b9ee100f9391cba66e9b3d641036f186a1b799
Kacper Grzelak <upsetboii@gmail.com>
2025-03-13 00:01:28 +0100

    recursive quick sort added

---
+++ quick_sort_r.py
@@ -0,0 +1,11 @@
+def quick_sort_r(array):
+    if len(array)<=1:
+        return array
+
+    pivot = array[len(array)-1]
+
+    left = [x for x in array if x > pivot]
+    middle = [x for x in array if x == pivot]
+    right = [x for x in array if x < pivot]
+
+    return quick_sort_r(left) + middle + quick_sort_r(right)
