895f08e19b75dd970b5c9c2c95648f9c9ac0da3a
Kacper Grzelak <upsetboii@gmail.com>
2025-03-17 15:46:39 +0100

    generate function fixed

--- generator.py
+++ generator.py
@@ -5,10 +5,6 @@ min_value = int(input("Podaj minimalną wartość losowanych elementów: "))
 max_value = int(input("Podaj maksymalną wartość losowanych elementów: "))
 
 def generate(n,min_value, max_value):
-    if(n > 10):
-        print("generator może stworzyć maksymalnie ciąg 10-elementowy.")
-        return []
-    
     if(min_value > max_value):
         print("minimalna wartość nie powinna być większa od maksymalnej.")
         return []
